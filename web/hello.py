5f7ad27cee216b2aa6ceeea37a9387f6b6ecb881
Danil Netsev <argetlmah@gmail.com>
2020-06-06 19:53:15 +0500

    test 1.9

--- web/hello.py
+++ web/hello.py
@@ -1,4 +1,5 @@
 def app(env, start_response):
     getparams = env['QUERY_STRING']
+    getcanc = getparams.split('&')
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return  [getparams]
+    return [bytes(x, 'utf-8') for x in getcanc]
