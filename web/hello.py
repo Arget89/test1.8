d309ad56902d751d7a690b69baed6ca6ee44cb7e
Danil Netsev <argetlmah@gmail.com>
2020-06-07 16:39:53 +0500

    added \n for response

--- web/hello.py
+++ web/hello.py
@@ -2,4 +2,4 @@ def app(env, start_response):
     getparams = env['QUERY_STRING']
     getcanc = getparams.split('&')
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [bytes(x, 'utf-8') for x in getcanc]
+    return [bytes(x+'\n', 'utf-8') for x in getcanc]
