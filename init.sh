72973c4e57b4ed54ef4a64b2fea773da6a54813a
Alexandr Netsev <netsev@yandex.ru>
2020-06-12 19:17:33 +0500

    Step2.1 commit

--- init.sh
+++ init.sh
@@ -3,11 +3,14 @@ cd /home/box/
 mv test1.8/* .
 #2. Удалить default в nginx site-enabled и сделать своего конфига
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/etc/nginx.conf /etc/nginx/sites-enabled/
+sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/
 # Указать конфиг для gunicorn
-sudo ln -s /home/box/etc/gunic.conf.py /etc/gunicorn.d/
+sudo ln -s /home/box/web/etc/gunic.conf.py /etc/gunicorn.d/
+sudo ln -s /home/box/web/etc/gunic2.conf.py /etc/gunicorn.d/
 # Запуск nginx
 sudo nginx
-# Запуск gunicorn
+Запуск gunicorn
 cd /home/box/web
-sudo gunicorn -c /etc/gunicorn.d/gunic.conf.py hello:app
+sudo gunicorn3 -c /etc/gunicorn.d/gunic.conf.py hello:app &
+cd /home/box/web/ask/
+sudo gunicorn3 -c /etc/gunicorn.d/gunic2.conf.py ask.wsgi:application &
