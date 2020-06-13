d0867c88be174b8318a8f73f6c1913529564e68d
Danil Netsev <argetlmah@gmail.com>
2020-06-13 17:54:34 +0500

    init rm - cp, and del link

--- init.sh
+++ init.sh
@@ -1,10 +1,13 @@
 #1. Переименовать и перенести внутренности из тест19 в box
 cd /home/box/
-mv test1.8/* .
+cp test1.8/* .
 #2. Удалить default в nginx site-enabled и сделать своего конфига
 sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-enabled/nginx.conf
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/
 # Указать конфиг для gunicorn
+sudo rm /etc/gunicorn.d/gunic.conf.py
+sudo rm /etc/gunicorn.d/gunic2.conf.py
 sudo ln -s /home/box/web/etc/gunic.conf.py /etc/gunicorn.d/
 sudo ln -s /home/box/web/etc/gunic2.conf.py /etc/gunicorn.d/
 # Запуск nginx
