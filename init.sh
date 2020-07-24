713363a91dc1d786483ebbc45071c77fa23fc906
Danil Netsev <argetlmah@gmail.com>
2020-07-24 15:51:44 +0500

    init change

--- init.sh
+++ init.sh
@@ -1,3 +1,21 @@
+sudo apt update
+sudo apt install python3.5
+sudo apt install python3.5-dev
+sudo unlink /usr/bin/python3
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+sudo python3 -m pip install gunicorn
+sudo python3 -m pip install django==2.0
+sudo python3 -m pip install mysqlclient
+
+sudo /etc/init.d/mysql start
+mysql -uroot -e "create database stepic_web;"
+mysql -uroot -e "grant all privileges on stepic_web.* to 'box'@'localhost' with grant option;"
+cd /web/ask/
+sudo python3 manage.py makemigrations
+cd /web/ask/
+sudo python3 manage.py migrate
+
+
 #1. Переименовать и перенести внутренности из тест19 в box
 cd /home/box/
 cp -r test1.8/* .
@@ -14,7 +32,7 @@ sudo ln -s /home/box/web/etc/gunic2.conf.py /etc/gunicorn.d/
 sudo nginx -s stop
 sudo nginx
 # Запуск gunicorn
-#cd /home/box/web
-#sudo gunicorn -c /etc/gunicorn.d/gunic.conf.py hello:app &
-#cd /home/box/web/ask/
-#sudo gunicorn -c /etc/gunicorn.d/gunic2.conf.py ask.wsgi:application &
+cd /home/box/web
+sudo gunicorn -c /etc/gunicorn.d/gunic.conf.py hello:app &
+cd /home/box/web/ask/
+sudo gunicorn -c /etc/gunicorn.d/gunic2.conf.py ask.wsgi:application &
